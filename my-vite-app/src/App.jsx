8820bbeb0e0fa074d90b5831168bf8728a42d36b
silvi <maheshwarisilvi98@gmail.com>
2025-02-09 16:06:25 +0530

    hh

---
+++ my-vite-app/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+ <Routes>
+  <Route>
+
+    
+  </Route>
+ </Routes>
+
+
+     </>
+  )
+}
+
+export default App
